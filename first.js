17b1190a77f17962e707ff0d12f0e55f5a8ea641
kimeu254 <jimkimeu254@gmail.com>
2022-07-29 12:06:30 +0300

    js concept

---
+++ first.js
@@ -0,0 +1,31 @@
+//Variables
+//using var
+
+var x
+var y 
+var z
+
+x = 5
+y = 10
+z = x + y
+
+console.log(z)
+
+//using let
+
+let num = 12
+let name = 'Jimmy'
+console.log(num)
+console.log(name)
+
+//using const
+
+var area
+const pi = 3.142
+let radius = 12
+
+area = pi * radius * radius
+
+console.log(area)
+
+
