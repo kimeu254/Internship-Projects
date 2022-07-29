7d7179eecf7ee973165ee9e9e851f4e9f4303b3f
kimeu254 <jimkimeu254@gmail.com>
2022-07-29 16:24:45 +0300

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+   main: "./src/index.js", 
+   vendor: "./src/vendor.js",
+  }, 
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
+  module: {
+    rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                'style-loader', //3. Injects styles into DOM
+                'css-loader',   //2. Turns css into commonjs
+                'sass-loader',  //1. Turns sass into css
+            ]
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.(svg|png|jpe?g|gif)$/i,
+          use: [
+            {  
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash].[ext]',
+                outputPath: "img",
+              }
+            }
+          ]
+        }
+    ]
+  }
+};
