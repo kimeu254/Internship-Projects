234182bd799ea9896e16038b16979662fd24fa56
kimeu254 <jimkimeu254@gmail.com>
2022-08-01 12:46:34 +0300

    vuejs day 1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ Title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      Title: "This is my first vue project",
+    };
+  },
+  
 }
 </script>
 
