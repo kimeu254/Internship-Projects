0d626cd1b04c5603ca39201ea04efcdcfb7a8faa
kimeu254 <jimkimeu254@gmail.com>
2022-08-04 09:38:07 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <Article id="title" :likes="100" :isPublished="true" />
+
+</template>
+
+<script>
+import Article from './components/Article.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //Greet, 
+    Article,
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>
+        Hello there {{ name }},
+    </h2>
+
+    
+</template>
+
+<script>
+    export default {
+        name: 'GreetInfo',
+        props: ['name'],
+    };
+</script>
+
+<style scoped>
+
+</style>
